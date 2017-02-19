7e3281c657356ca1b2b54fe17ddf9ff6acca0e1b
onasenko-sergey <onasenko.sergey@gmail.com>
2017-02-19 23:01:48 +0300

    remove starter kit default components

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,14 @@
 // We only need to import the modules necessary for initial render
 import CoreLayout from '../layouts/CoreLayout'
-import Home from './Home'
-import CounterRoute from './Counter'
 
 /*  Note: Instead of using JSX, we recommend using react-router
     PlainRoute objects to build route definitions.   */
 
 export const createRoutes = (store) => ({
   path        : '/',
-  component   : CoreLayout,
-  indexRoute  : Home,
-  childRoutes : [
-    CounterRoute(store)
-  ]
+  component   : CoreLayout
+  // indexRoute  :
+  // childRoutes : []
 })
 
 /*  Note: childRoutes can be chunked or otherwise loaded programmatically
